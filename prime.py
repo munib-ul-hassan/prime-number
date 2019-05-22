1a5393b3fc0a36874e892420093d9bb0faef0c04
munib-ul-hassan <46391010+munib-ul-hassan@users.noreply.github.com>
2019-05-22 16:43:58 -0700

    Delete prime.py

--- prime.py
+++
@@ -1,8 +0,0 @@
-a = int (input ("Input your value: "))
-b = 0
-for i in range (2,a-1):
-    if (a % i) == 0:
-        print ("this is not a prie number")
-        break
-else:
-    print ("this is  prime number")
